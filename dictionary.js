28a7f23ec34e707c01ac693b77f27f74194caaaa
Mohamad Ramdan Firdaus <ramdan@192.168.1.6>
2023-06-30 05:47:23 +0700

    dict - one asterix

--- dictionary.js
+++ dictionary.js
@@ -1,10 +1,12 @@
 class Dictionary {
 	constructor(words) {
-		this.dict = new Set(words)
+		this.dict = words
 	}
 
 	isInDict(word) {
-		return this.dict.has(word)
+		const template = word.replace('*', '.')
+		const reg = new RegExp(template)
+		return this.dict.some(w => reg.test(w))
 	}
 }
 
@@ -13,3 +15,4 @@ const test = new Dictionary(['cat', 'car', 'plan'])
 console.log(test.isInDict('cat')) // true
 console.log(test.isInDict('car')) // true
 console.log(test.isInDict('joe')) // false
+console.log(test.isInDict('*at')) // true
